0222e2a9bd3861abf877f9c95991cbcb70f4cc0e
gilmarinoj <gilmarinoj@gmail.com>
2025-03-25 17:26:22 -0400

    added react components

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,22 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import auth from 'auth-astro';
+
+import tailwindcss from '@tailwindcss/vite';
+
+import netlify from '@astrojs/netlify';
+
+import db from '@astrojs/db';
+
+import react from '@astrojs/react';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [auth(), db(), react()],
+  output: 'server',
+  vite: {
+    plugins: [tailwindcss()]
+  },
+  adapter: netlify()
+});
---
+++ src/actions/index.ts
@@ -0,0 +1,14 @@
+import { loginUser, logout, registerUser } from './auth';
+import { getProductsByPage } from './products/get-products-by-page.action';
+
+export const server = {
+  // actions
+
+  // Auth
+  loginUser,
+  logout,
+  registerUser,
+
+  // Products
+  getProductsByPage
+};
